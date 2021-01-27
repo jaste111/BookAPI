c9353f47ad3c4860dca3e7206741c88ae02f53be
jaste111 <jan.steimann@hhu.de>
2021-01-27 10:45:38 +0100

    Save json array in books variable

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = 3000;
 
 var rawdata = fs.readFileSync('books.json');
 var books = JSON.parse(rawdata);
+var books = books.books;
 
 app.listen(port, function(){
     console.log("Server is listening on port: " + port);
