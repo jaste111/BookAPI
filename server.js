d20eb8b52657aed31a033621ebf0108cdae922a5
jaste111 <jan.steimann@hhu.de>
2021-01-27 10:37:54 +0100

    Add server setup and read JSON file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+
+const app = express();
+
+const port = 3000;
+
+var rawdata = fs.readFileSync('books.json');
+var books = JSON.parse(rawdata);
+
+app.listen(port, function(){
+    console.log("Server is listening on port: " + port);
+});
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/books.json");
+});
