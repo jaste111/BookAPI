17917b8f4fbd0afa64b87689e238ceab80dada48
jaste111 <jan.steimann@hhu.de>
2021-01-30 20:13:16 +0100

    Add PUT route

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const { stringify } = require("querystring");
 
 const app = express();
 const port = 3000;
-const bookFile = "books.json";
+const bookFileName = "books.json";
 
 app.use(bodyParser.json());
 
-var rawdata = fs.readFileSync(bookFile);
+var rawdata = fs.readFileSync(bookFileName);
 var books = JSON.parse(rawdata);
 
 app.listen(port, function () {
@@ -36,6 +37,21 @@ app.post("/", function (req, res) {
     books.push(newBook);
     console.log(books);
     var newBookData = JSON.stringify(books, null, 2);
-    fs.writeFileSync(bookFile, newBookData);
+    fs.writeFileSync(bookFileName, newBookData);
   }
+});
+
+app.put("/", function(req, res){
+    res.send("Got a PUT request");
+    const updatedBook = req.body;
+    const idOfBook = updatedBook.id;
+   
+    const indexOfBook = books.findIndex((el) => el.id ===  idOfBook);
+    if(indexOfBook === -1){
+        res.status(400).send("ID does not exist");
+    }else{
+        books[indexOfBook] = updatedBook;
+        var booksData = JSON.stringify(books, null, 2);
+        fs.writeFileSync(bookFileName, booksData);
+    }
 });
